8401ab2a9b74e606c519880e5f9f6eb256f42b5d
Areen <Areen.Rabieh@gmail.com>
2022-06-08 20:21:59 +0200

    Signed-off-by: Areen <Areen.Rabieh@gmail.com>

--- file_01.js
+++ file_01.js
@@ -1,7 +1,7 @@
-var http = require('http');
+import readline from 'readline';
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
-
+var score = parseInt(readLine(), 10)
+if (score >= 88) console.log("excellent");
+    else if (score >= 40 && score < 88)
+        console.log("good");
+    else console.log("fail");
