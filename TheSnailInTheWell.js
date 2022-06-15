7299e6ed3da6b256827a6bd5af507db547ffbba1
Areen15 <106708038+Areen15@users.noreply.github.com>
2022-06-15 19:29:48 +0200

    Add files via upload

---
+++ TheSnailInTheWell.js
@@ -0,0 +1,22 @@
+    const prompt = require('prompt-sync')({sigint: true});
+
+
+    let depth = prompt('Enter a number: ');   //42
+    let days = 0;
+
+
+    for (let i = 0; depth >= 5; i++) 
+    {
+        depth = depth - 5;
+        days++;
+
+      }
+
+
+    if (depth >= 2.5)
+    {
+        days++
+    }
+     
+    console.log("days = ",days);
+
